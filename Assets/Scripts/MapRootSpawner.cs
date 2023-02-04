00e0e449b370f233e2b1c2ab4cfb02ee50f7a064
Gonzalo Perez de la Ossa <dense13@gmail.com>
2023-02-05 10:41:08 +1100

    Makes all initial Roots static

--- Assets/Scripts/MapRootSpawner.cs
+++ Assets/Scripts/MapRootSpawner.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class MapRootSpawner : MonoBehaviour
 {
-    public HexGrid Map;
     public GameObject MainRootPrefab;
 
     [SerializeField] float timeBetweenSpawns;
@@ -12,16 +11,6 @@ public class MapRootSpawner : MonoBehaviour
     private void Awake()
     {
         timeToNextSpawn = timeBetweenSpawns;
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        SpawnAt(0,0);
-        SpawnAt(Map.chunkCountX * HexMetrics.ChunkSizeX - 1,0);
-        SpawnAt(0,Map.chunkCountZ * HexMetrics.ChunkSizeZ - 1);
-        SpawnAt(Map.chunkCountX * HexMetrics.ChunkSizeX - 1,Map.chunkCountZ * HexMetrics.ChunkSizeZ - 1);
     }
 
 
@@ -36,16 +25,6 @@ public class MapRootSpawner : MonoBehaviour
         {
             timeToNextSpawn -= Time.deltaTime;
         } 
-    }
-
-
-    void SpawnAt(int x, int z)
-    {
-        var hexCenter = Map.transform.position + HexMetrics.GetCenter(x, z);
-
-        GameObject newGO = Instantiate(MainRootPrefab, hexCenter, Quaternion.identity);
-        newGO.transform.LookAt(Vector3.zero);
-        newGO.GetComponent<RootSegment>().SetHealth(100f);
     }
 
 
